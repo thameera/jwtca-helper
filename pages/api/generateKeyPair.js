f1e370688dee57aecb47cebe3eb974df3a056417
Thameera Senanayaka <me@thameera.com>
2024-05-24 23:06:13 +1000

    Promisify generateKeyPair call to avoid warning
    This fixes the following warning from Next.js:
    API resolved without sending a response for /api/generateKeyPair, this may result in stalled requests.

--- pages/api/generateKeyPair.js
+++ pages/api/generateKeyPair.js
@@ -1,26 +1,27 @@
 const crypto = require('crypto')
+const promisify = require('util').promisify
 
-export default function handler(req, res) {
-  crypto.generateKeyPair('rsa', {
-    modulusLength: 4096,
-    publicKeyEncoding: {
-      type: 'spki',
-      format: 'pem'
-    },
-    privateKeyEncoding: {
-      type: 'pkcs8',
-      format: 'pem',
-    }
-  }, (err, publicKey, privateKey) => {
-    if (err) {
-      console.log(err)
-      res.send(401).json({ err: 'Error creating key pair' })
-      return
-    }
+const generateKeyPairAsync = promisify(crypto.generateKeyPair)
 
+export default async function handler(req, res) {
+  try {
+    const { publicKey, privateKey } = await generateKeyPairAsync('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+      }
+    })
     res.json({
       publicKey,
       privateKey
     })
-  })
+  } catch (err) {
+    console.log(err)
+    res.send(401).json({ err: 'Error creating key pair' })
+  }
 }
